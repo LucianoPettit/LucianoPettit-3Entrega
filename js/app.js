5b3a9434e3af7bec46ef4405062496017660d5b4
¨luciano <lucianopettit@gmail.com>
2023-03-22 02:06:52 -0300

    Update administrarProductos.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
-class ProductoController{
-    constructor(){
-        this.listaProductos = []
-    }
+class ProductoController {
+  constructor() {
+    this.listaProductos = [];
+  }
 
-    levantar(){
-        let obtenerListaJSON = localStorage.getItem("listaProductos")
+  levantar() {
+    let obtenerListaJSON = localStorage.getItem("listaProductos");
 
-        if(obtenerListaJSON){
-            this.listaProductos = JSON.parse(obtenerListaJSON)
-        }
+    if (obtenerListaJSON) {
+      this.listaProductos = JSON.parse(obtenerListaJSON);
     }
-    
-    mostrarEnDOM(contenedor_productos){
-        //limpio el dom 
-        contenedor_productos.innerHTML = ""
-        //muestra  la lista completa
-        this.listaProductos.forEach( producto => {
-            contenedor_productos.innerHTML += `
+  }
+
+  mostrarEnDOM(contenedor_productos) {
+    //limpio el dom
+    contenedor_productos.innerHTML = "";
+    //muestra  la lista completa
+    this.listaProductos.forEach((producto) => {
+      contenedor_productos.innerHTML += `
             <div class="card" style="width: 18rem;">
                 <img src="${producto.img}" class="card-img-top" alt="${producto.alt}">
                 <div class="card-body">
@@ -30,36 +30,36 @@ class ProductoController{
                     <a href="#" class="btn btn-primary" id="cpu${producto.id}">Añadir al carrito</a>
                 </div>
             </div>
-            `
-        })
-    }
+            `;
+    });
+  }
 }
 
-class CarritoController{
-    constructor(){
-        this.listaCarrito = []
+class CarritoController {
+  constructor() {
+    this.listaCarrito = [];
+  }
+
+  levantar() {
+    let obtenerListaJSON = localStorage.getItem("listaCarrito");
+
+    if (obtenerListaJSON) {
+      this.listaCarrito = JSON.parse(obtenerListaJSON);
     }
+  }
 
-    levantar(){
-        let obtenerListaJSON = localStorage.getItem("listaCarrito")
+  anadir(producto) {
+    this.listaCarrito.push(producto);
+    let arrFormatoJSON = JSON.stringify(this.listaCarrito);
+    localStorage.setItem("listaCarrito", arrFormatoJSON);
+  }
 
-        if(obtenerListaJSON){
-            this.listaCarrito = JSON.parse(obtenerListaJSON)
-        }
-    }
-
-    anadir(producto){
-        this.listaCarrito.push(producto)
-        let arrFormatoJSON = JSON.stringify(this.listaCarrito)
-        localStorage.setItem("listaCarrito",arrFormatoJSON)
-    }
-
-    mostrarEnDOM(contenedor_carrito){
-        //limpio el container
-        contenedor_carrito.innerHTML = ""
-        //muestro todo 
-        this.listaCarrito.forEach( producto => {
-            contenedor_carrito.innerHTML += `
+  mostrarEnDOM(contenedor_carrito) {
+    //limpio el container
+    contenedor_carrito.innerHTML = "";
+    //muestro todo
+    this.listaCarrito.forEach((producto) => {
+      contenedor_carrito.innerHTML += `
             <div class="card mb-3" style="max-width: 540px;">
                 <div class="row g-0">
                     <div class="col-md-4">
@@ -76,35 +76,91 @@ class CarritoController{
                     </div>
                 </div>
             </div>
-            `
-        })
-    }
+            `;
+    });
+  }
+
+  limpiar(){
+    this.listaCarrito = []
+    localStorage.removeItem("listaCarrito")
+
+
+
+  }
 }
 
 //Objetos controladores
-const controladorProductos = new ProductoController()
-const controladorCarrito = new CarritoController()
+const controladorProductos = new ProductoController();
+const controladorCarrito = new CarritoController();
 
 //Verificar STORAGE
-controladorProductos.levantar()
-controladorCarrito.levantar()
+controladorProductos.levantar();
+controladorCarrito.levantar();
 
 //DOM
-const contenedor_productos = document.getElementById("contenedor_productos")
-const contenedor_carrito = document.getElementById("contenedor_carrito")
+const contenedor_productos = document.getElementById("contenedor_productos");
+const contenedor_carrito = document.getElementById("contenedor_carrito");
+const finalizar_compra = document.getElementById("finalizar_compra");
+const vaciar_carrito = document.getElementById("vaciar_carrito");
 
 //APP JS
-controladorProductos.mostrarEnDOM(contenedor_productos)
-controladorCarrito.mostrarEnDOM(contenedor_carrito)
+controladorProductos.mostrarEnDOM(contenedor_productos);
+controladorCarrito.mostrarEnDOM(contenedor_carrito);
 
 //Añadimos Eventos a los botones de cada CARD
-controladorProductos.listaProductos.forEach( producto => {
-    const productoEnEsperaDeSerAnadido = document.getElementById(`cpu${producto.id}`)
+controladorProductos.listaProductos.forEach((producto) => {
+  const productoEnEsperaDeSerAnadido = document.getElementById(
+    `cpu${producto.id}`
+  );
 
-    productoEnEsperaDeSerAnadido.addEventListener("click",()=>{
-        
-        controladorCarrito.anadir(producto)
-        controladorCarrito.levantar()
-        controladorCarrito.mostrarEnDOM(contenedor_carrito)
-    })
+  productoEnEsperaDeSerAnadido.addEventListener("click", () => {
+    controladorCarrito.anadir(producto);
+    controladorCarrito.levantar();
+    controladorCarrito.mostrarEnDOM(contenedor_carrito);
+    Toastify({
+        text: "Producto añadido con éxito",
+        duration: 3000,
+        gravity: "bottom", 
+        position: "right", 
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        }
+      }).showToast();
+
+  });
+});
+
+finalizar_compra.addEventListener("click", () => {
+    
+
+    if(controladorCarrito.listaCarrito.length > 0 ){
+    controladorCarrito.limpiar()
+    controladorCarrito.mostrarEnDOM(contenedor_carrito)
+     
+
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Compra realizada con éxito!",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+}else{
+    Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Debe agregar al menos un producto al carrito.",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+}
+
+
+
+    
 })
+
+vaciar_carrito.addEventListener("click", () => {
+    
+    controladorCarrito.limpiar()
+    controladorCarrito.mostrarEnDOM(contenedor_carrito)})
