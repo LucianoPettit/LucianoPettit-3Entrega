7a0bbe4226bac7b912bcc939467b1c913d23b6b4
¨luciano <lucianopettit@gmail.com>
2023-04-03 18:15:40 -0300

    testeo

--- app.js
+++ app.js
@@ -254,3 +254,38 @@ vaciar_carrito.addEventListener("click", () => {
     
     controladorCarrito.limpiar()
     controladorCarrito.mostrarEnDOM(contenedor_carrito)})
+
+
+
+
+    let listado = document.getElementById("listado");
+
+    fetch('../data.json')
+    .then(response => response.json())
+    .then(data => {
+      
+      data.forEach(producto => {
+        const div = document.createElement("div");
+        div.classList.add("col-sm-4", "col");
+        div.innerHTML = `
+    
+    <div class="card" style="width: 18rem;">
+    <img src="${producto.img}" class="card-img-top" alt="${producto.alt}">
+    <div class="card-body">
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">
+            ${producto.descripcion}
+        </p>
+     
+        <a href="#" class="btn btn-primary" id="vaper${producto.id}">visitar producto</a>
+    </div>
+    </div>
+    
+    `
+    
+    
+        ;
+    
+        listado.append(div);
+      });
+    })
