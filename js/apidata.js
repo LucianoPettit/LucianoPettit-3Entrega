2859f06bb888f15e9203d9f83e49f25bb2351b63
¨luciano <lucianopettit@gmail.com>
2023-04-03 19:13:20 -0300

    entrega final

---
+++ js/apidata.js
@@ -0,0 +1,31 @@
+
+let listado = document.getElementById("listado");
+
+fetch("../js/data.json")
+.then(response => response.json())
+.then(data => {
+  data.forEach(producto => {
+    const div = document.createElement("div");
+    div.classList.add("col-sm-4", "col");
+    div.innerHTML = `
+
+<div class="card" style="width: 18rem;">
+<img src="${producto.img}" class="card-img-top" alt="${producto.alt}">
+<div class="card-body">
+    <h5 class="card-title">${producto.nombre}</h5>
+    <p class="card-text">
+        ${producto.descripcion}
+    </p>
+ 
+    <a href="#" class="btn btn-primary" id="vaper${producto.id}">visitar producto</a>
+</div>
+</div>
+
+`
+
+
+    ;
+
+    listado.append(div);
+  });
+})
--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@ class ProductoController {
 
     if (obtenerListaJSON) {
       this.listaProductos = JSON.parse(obtenerListaJSON);
-      return true
+      return true;
     }
-    return false 
+    return false;
   }
 
   mostrarEnDOM(contenedor_productos) {
@@ -40,51 +40,45 @@ class ProductoController {
 class CarritoController {
   constructor() {
     this.listaCarrito = [];
-    this.precio = document.getElementById("precio"),
-    this.precio_con_iva = document.getElementById("precio_con_iva")
-                        
+    (this.precio = document.getElementById("precio")),
+      (this.precio_con_iva = document.getElementById("precio_con_iva"));
   }
 
- borrar(producto){
-
-  let indice = this.listaCarrito.indexOf(producto)
-  this.listaCarrito.splice(indice,1)
-
- }
+  borrar(producto) {
+    let indice = this.listaCarrito.indexOf(producto);
+    this.listaCarrito.splice(indice, 1);
+  }
 
   levantar() {
     let obtenerListaJSON = localStorage.getItem("listaCarrito");
 
     if (obtenerListaJSON) {
       this.listaCarrito = JSON.parse(obtenerListaJSON);
-
     }
   }
 
   anadir(producto) {
-  let existeProducto = this.listaCarrito.some(elemento => elemento.id == producto.id ) 
+    let existeProducto = this.listaCarrito.some(
+      (elemento) => elemento.id == producto.id
+    );
 
-    if (existeProducto){
-
-      const productoEncontrado = this.buscar(producto.id)
-      productoEncontrado.cantidad += 1
-    }
-    else{
+    if (existeProducto) {
+      const productoEncontrado = this.buscar(producto.id);
+      productoEncontrado.cantidad += 1;
+    } else {
       this.listaCarrito.push(producto);
-
     }
 
     let arrFormatoJSON = JSON.stringify(this.listaCarrito);
     localStorage.setItem("listaCarrito", arrFormatoJSON);
   }
 
-
-mostrarEnDOM(contenedor_carrito) {
-  //limpio el container
-  contenedor_carrito.innerHTML = "";
-  //muestro todo
-  this.listaCarrito.forEach((producto) => {
-    contenedor_carrito.innerHTML += `
+  mostrarEnDOM(contenedor_carrito) {
+    //limpio el container
+    contenedor_carrito.innerHTML = "";
+    //muestro todo
+    this.listaCarrito.forEach((producto) => {
+      contenedor_carrito.innerHTML += `
           <div class="card mb-3" style="max-width: 540px;">
               <div class="row g-0">
                   <div class="col-md-4">
@@ -102,33 +96,40 @@ mostrarEnDOM(contenedor_carrito) {
               </div>
           </div>
           `;
-          document.getElementById(`borrar${producto.id}`).addEventListener("click", () => {
-            // borramos el producto de this.listaProductos
-           this.borrar(producto)
+      document
+        .getElementById(`borrar${producto.id}`)
+        .addEventListener("click", () => {
+          // borramos el producto de this.listaProductos
+          this.borrar(producto);
           //  actualizamos el storage
-           localStorage.setItem("listaCarrito", JSON.stringify(this.listaCarrito))
+          localStorage.setItem(
+            "listaCarrito",
+            JSON.stringify(this.listaCarrito)
+          );
           // actualizamos el dom
-          this.mostrarEnDOM(contenedor_carrito)
-          this.mostrarPreciosEnDOM()
-        } )
-    document.getElementById(`borrar${producto.id}`).addEventListener("click", () => {
-      this.borrar(producto);
+          this.mostrarEnDOM(contenedor_carrito);
+          this.mostrarPreciosEnDOM();
+        });
+      document
+        .getElementById(`borrar${producto.id}`)
+        .addEventListener("click", () => {
+          this.borrar(producto);
+        });
     });
-  });
-}
-
-
-mostrarPreciosEnDOM(){
- this.precio.innerHTML = this.calcularTotal()
- this.precio_con_iva.innerHTML = this.calcularPrecioConIva()
-  
-}
-
-  calcularTotal(){
-   return this.listaCarrito.reduce((acumulador,producto)=> acumulador + producto.precio * producto.cantidad ,0)
   }
 
+  mostrarPreciosEnDOM() {
+    this.precio.innerHTML = this.calcularTotal();
+    this.precio_con_iva.innerHTML = this.calcularPrecioConIva();
+  }
 
+  calcularTotal() {
+    return this.listaCarrito.reduce(
+      (acumulador, producto) =>
+        acumulador + producto.precio * producto.cantidad,
+      0
+    );
+  }
 
   //   calcularPrecioConIva(){
   //  return this.calcularTotal() * 1.21
@@ -136,31 +137,32 @@ mostrarPreciosEnDOM(){
 
   calcularPrecioConIva() {
     const total = this.calcularTotal() * 1.21;
-    return Number(total.toFixed()); 
+    return Number(total.toFixed());
   }
-  
- 
 
-    buscar(id){
-    return this.listaCarrito.find(producto => producto.id == id)
-    }
+  buscar(id) {
+    return this.listaCarrito.find((producto) => producto.id == id);
+  }
 
-  limpiar(){
-    this.listaCarrito = []
-    localStorage.removeItem("listaCarrito")
+  limpiar() {
+    this.listaCarrito = [];
+    localStorage.removeItem("listaCarrito");
 
-
-    this.listaCarrito.forEach( producto => {
-
-        document.getElementById(`borrar${producto.id}`).addEventListener("click", () => {
-            // borramos el producto de this.listaProductos
-           this.borrar(producto)
+    this.listaCarrito.forEach((producto) => {
+      document
+        .getElementById(`borrar${producto.id}`)
+        .addEventListener("click", () => {
+          // borramos el producto de this.listaProductos
+          this.borrar(producto);
           //  actualizamos el storage
-           localStorage.setItem("listaCarrito", JSON.stringify(this.listaCarrito))
+          localStorage.setItem(
+            "listaCarrito",
+            JSON.stringify(this.listaCarrito)
+          );
           // actualizamos el dom
-          this.mostrarEnDOM(contenedor_carrito)
-        } )
-    })
+          this.mostrarEnDOM(contenedor_carrito);
+        });
+    });
   }
 }
 
@@ -172,19 +174,14 @@ const controladorCarrito = new CarritoController();
 controladorProductos.levantar();
 const levantoAlgo = controladorCarrito.levantar();
 
-
-
 //DOM
 
 const finalizar_compra = document.getElementById("finalizar_compra");
 const vaciar_carrito = document.getElementById("vaciar_carrito");
 
-
-if (levantoAlgo){
-
-  controladorCarrito.mostrarPreciosEnDOM(precio,precio_con_iva)
+if (levantoAlgo) {
+  controladorCarrito.mostrarPreciosEnDOM(precio, precio_con_iva);
 }
-
 
 //APP JS
 controladorProductos.mostrarEnDOM(contenedor_productos);
@@ -196,7 +193,6 @@ controladorProductos.listaProductos.forEach((producto) => {
   const productoEnDOM = document.getElementById(`vaper${producto.id}`);
 
   productoEnDOM.addEventListener("click", () => {
-
     controladorCarrito.anadir(producto);
 
     controladorCarrito.levantar();
@@ -205,87 +201,43 @@ controladorProductos.listaProductos.forEach((producto) => {
     controladorCarrito.mostrarPreciosEnDOM(precio, precio_con_iva);
 
     Toastify({
-        text: "Producto añadido con éxito",
-        duration: 3000,
-        gravity: "bottom", 
-        position: "right", 
-        style: {
-          background: "linear-gradient(to right, #00b09b, #96c93d)",
-        }
-      }).showToast();
-
+      text: "Producto añadido con éxito",
+      duration: 3000,
+      gravity: "bottom",
+      position: "right",
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+    }).showToast();
   });
 });
 
 finalizar_compra.addEventListener("click", () => {
-    
-   
+  if (controladorCarrito.listaCarrito.length > 0) {
+    controladorCarrito.limpiar();
+    controladorCarrito.mostrarEnDOM(contenedor_carrito);
+    controladorCarrito.mostrarPreciosEnDOM(precio, precio_con_iva);
 
-    if(controladorCarrito.listaCarrito.length > 0 ){
-    controladorCarrito.limpiar()
-    controladorCarrito.mostrarEnDOM(contenedor_carrito)
-     controladorCarrito.mostrarPreciosEnDOM(precio,precio_con_iva)
-
-  Swal.fire({
-    position: "top-end",
-    icon: "success",
-    title: "Compra realizada con éxito!",
-    showConfirmButton: false,
-    timer: 1500,
-  });
-}else{
     Swal.fire({
-        position: "center",
-        icon: "error",
-        title: "Debe agregar al menos un producto al carrito.",
-        showConfirmButton: false,
-        timer: 2000,
-      });
-}
-
-
-
-
-
-    
-})
+      position: "top-end",
+      icon: "success",
+      title: "Compra realizada con éxito!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } else {
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "Debe agregar al menos un producto al carrito.",
+      showConfirmButton: false,
+      timer: 2000,
+    });
+  }
+});
 
 vaciar_carrito.addEventListener("click", () => {
-    
-    controladorCarrito.limpiar()
-    controladorCarrito.mostrarEnDOM(contenedor_carrito)})
+  controladorCarrito.limpiar();
+  controladorCarrito.mostrarEnDOM(contenedor_carrito);
+});
 
-
-
-
-    let listado = document.getElementById("listado");
-
-    fetch('../data.json')
-    .then(response => response.json())
-    .then(data => {
-      
-      data.forEach(producto => {
-        const div = document.createElement("div");
-        div.classList.add("col-sm-4", "col");
-        div.innerHTML = `
-    
-    <div class="card" style="width: 18rem;">
-    <img src="${producto.img}" class="card-img-top" alt="${producto.alt}">
-    <div class="card-body">
-        <h5 class="card-title">${producto.nombre}</h5>
-        <p class="card-text">
-            ${producto.descripcion}
-        </p>
-     
-        <a href="#" class="btn btn-primary" id="vaper${producto.id}">visitar producto</a>
-    </div>
-    </div>
-    
-    `
-    
-    
-        ;
-    
-        listado.append(div);
-      });
-    })
